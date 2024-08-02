4ed24e45427af7df9cf292072650d039823cb14d
marcioHcastilho <marciocastilho@bemprotege.com.br>
2024-08-02 17:41:12 -0300

    bug correct

--- app/services/user_authentication_service.rb
+++ app/services/user_authentication_service.rb
@@ -1,16 +1,16 @@
 class UserAuthenticationService
-    def initialize(email, password)
-      @user = User.find_by(email: email)
-      @password = password
-    end
-  
-    def call
-      if @user && @user.authenticate(@password)
-        token = JsonWebToken.encode(user_id: @user.id)
-        { success: true, token: token, user: @user }
-      else
-        { success: false, errors: ["Invalid email or password"] }
-      end
+  def initialize(email, password)
+    @user = User.find_by(email: email)
+    @password = password
+  end
+
+  def call
+    if @user && @user.authenticate(@password)
+      token = JsonWebToken.encode(user_id: @user.id)
+      { success: true, token: token, user: @user }
+    else
+      { success: false, errors: ["Invalid email or password"] }
     end
   end
+end
   
